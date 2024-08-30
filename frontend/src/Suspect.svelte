646cb1003d6787e7e8f737ffbc0a7113037b3de3
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-08-30 11:16:12 +0200

    Style the Suspects flex box and Suspect items basically

--- frontend/src/Suspect.svelte
+++ frontend/src/Suspect.svelte
@@ -9,8 +9,16 @@
     }
 </script>
 
-<div class="suspect" id={susID} on:click={selected} on:keydown={selected}>
-    <img src={imgSrc} alt="Suspect">
-</div>
+<div class="suspect" id={susID} on:click={selected} on:keydown={selected} style="background-image: url({imgSrc});"></div>
 
-<style></style>
+<style>
+    .suspect {
+        height: 21vh;
+        width: 21vh;
+        margin: 1%;
+        background-size: cover; /* Cover to ensure the image covers the entire div */
+        background-position: center; /* Center the image */
+        background-repeat: no-repeat; /* Prevent the image from repeating */
+    }
+
+</style>
---
+++ frontend/src/Suspects.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Suspect from './Suspect.svelte'
+</script>
+
+<div class="suspects">
+    <Suspect susID="sus1" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus2" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus3" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus4" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus5" imgSrc="src/assets/images/suspects/1.jpg"/>
+
+    <Suspect susID="sus6" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus7" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus8" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus9" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus10" imgSrc="src/assets/images/suspects/2.jpg"/>
+    
+    <Suspect susID="sus11" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus12" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus13" imgSrc="src/assets/images/suspects/1.jpg"/>
+    <Suspect susID="sus14" imgSrc="src/assets/images/suspects/2.jpg"/>
+    <Suspect susID="sus15" imgSrc="src/assets/images/suspects/1.jpg"/>
+</div>
+
+<style>
+    .suspects {
+        justify-content: flex-start;
+        display: flex;
+        flex-wrap: wrap;
+        width: 120vh;
+        max-width: 100vw;
+        max-height: 80vh;
+        flex-shrink: 1;
+    }
+</style>
