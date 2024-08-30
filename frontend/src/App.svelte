88aea7a8d6dcd7a3f7e5b9446e5b65c75a870608
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-08-30 10:22:51 +0200

    Init some basic template, lets go Svelte!

---
+++ frontend/src/App.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import logo from './assets/images/logo-universal.png'
+  import Suspect from './Suspect.svelte'
+
+  import {Greet} from '../wailsjs/go/main/App.js'
+
+  let resultText: string = "Please enter your name below 👇"
+  let name: string
+
+  function greet(): void {
+    Greet(name).then(result => resultText = result)
+  }
+</script>
+
+<main>
+  <div class="result" id="result">{resultText}</div>
+  <div class="input-box" id="input">
+    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
+    <button class="btn" on:click={greet}>Greet</button>
+  </div>
+
+  <Suspect susID="susA" imgSrc="src/assets/images/suspects/1.jpg"/>
+  <Suspect susID="susB" imgSrc="src/assets/images/suspects/2.jpg"/>
+</main>
+
+<style>
+
+  .result {
+    height: 20px;
+    line-height: 20px;
+    margin: 1.5rem auto;
+  }
+
+  .input-box .btn {
+    width: 60px;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 3px;
+    border: none;
+    margin: 0 0 0 20px;
+    padding: 0 8px;
+    cursor: pointer;
+  }
+
+  .input-box .btn:hover {
+    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
+    color: #333333;
+  }
+
+  .input-box .input {
+    border: none;
+    border-radius: 3px;
+    outline: none;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 10px;
+    background-color: rgba(240, 240, 240, 1);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  .input-box .input:hover {
+    border: none;
+    background-color: rgba(255, 255, 255, 1);
+  }
+
+  .input-box .input:focus {
+    border: none;
+    background-color: rgba(255, 255, 255, 1);
+  }
+
+</style>
---
+++ frontend/src/Suspect.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let susID: string;
+    export let imgSrc: string;
+
+    import logo from './assets/images/logo-universal.png'
+    
+    function selected() {
+        console.log("Suspect selected!")
+    }
+</script>
+
+<div class="suspect" id={susID} on:click={selected} on:keydown={selected}>
+    <img src={imgSrc} alt="Suspect">
+</div>
+
+<style></style>
