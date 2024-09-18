eda33fb310e167a188e9bff87d102385aa6c1932
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-09-18 14:58:29 +0200

    Add utils.ImageToBase64()

--- database/utils.go
+++ database/utils.go
@@ -1,12 +1,43 @@
 package database
 
-import "time"
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
 
 const (
 	TimeFormat string = time.RFC3339Nano
 )
 
-// Get Timestamp of current time.
+// Get Timestamp of current time in format unified for the whole program - time.RFC3339Nano.
 func TimestampNow() string {
 	return time.Now().Format(TimeFormat)
+}
+
+// Get the base46 string of the image at the specified imagePath.
+func ImageToBase64(imagePath string) (string, error) {
+	ext := filepath.Ext(imagePath)
+	ext = strings.ToLower(ext)
+	if ext != ".jpg" && ext != ".jpeg" {
+		return "", fmt.Errorf("unsupported image format: only JPEG files are allowed")
+	}
+
+	imgFile, err := os.Open(imagePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open image: %v", err)
+	}
+	defer imgFile.Close()
+
+	imageBytes, err := os.ReadFile(imagePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read image file: %v", err)
+	}
+
+	base64Image := base64.StdEncoding.EncodeToString(imageBytes)
+
+	return base64Image, nil
 }
