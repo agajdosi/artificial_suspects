85735288e74949d66d1063e80c7f31eb9518cfb5
Andreas Gajdosik <andreas@gajdosik.org>
2025-03-11 22:46:02 +0100

    Migrate few functions

--- backend/main.go
+++ backend/main.go
@@ -17,6 +17,9 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/new_game", NewGameHandler)
+	mux.HandleFunc("/current_game", GetCurrentGameHandler)
+	mux.HandleFunc("/next_investigation", NextInvestigationHandler)
+	mux.HandleFunc("/next_round", NextRoundHandler)
 	err = http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
 		log.Fatal(err)
@@ -53,6 +56,64 @@ func GetCurrentGameHandler(w http.ResponseWriter, r *http.Request) {
 	resp, err := json.Marshal(game)
 	if err != nil {
 		fmt.Println("GetCurrentGame() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func NextInvestigationHandler(w http.ResponseWriter, r *http.Request) {
+	game, err := database.GetCurrentGame()
+	if err != nil {
+		fmt.Println("NextInvestigation() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	game.Investigation, err = database.NewInvestigation(game.UUID)
+	if err != nil {
+		fmt.Println("NextInvestigation() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal(game)
+	if err != nil {
+		fmt.Println("NextInvestigation() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func NextRoundHandler(w http.ResponseWriter, r *http.Request) {
+	game, err := database.GetCurrentGame()
+	if err != nil {
+		fmt.Println("NextRound() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	round, err := database.NewRound(game.Investigation.UUID)
+	if err != nil {
+		fmt.Println("NextRound() error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	go database.GetAnswerFromAI(round, game.Investigation.CriminalUUID)
+
+	game.Investigation.Rounds = append(game.Investigation.Rounds, round) // prepend
+
+	fmt.Printf("New Round %d: %s\n", game.Level, game.Investigation.Rounds[len(game.Investigation.Rounds)-1].Question.English)
+
+	resp, err := json.Marshal(game)
+	if err != nil {
+		fmt.Println("NextRound() error:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
