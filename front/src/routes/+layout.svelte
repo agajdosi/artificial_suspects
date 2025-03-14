4f0daa41768fa33b28a4271ce97e4d7e356d092d
Andreas Gajdosik <andreas@gajdosik.org>
2025-03-14 17:36:35 +0100

    Implement basic global styling

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -26,12 +26,53 @@
     font-style: normal;
     font-weight: 400;
     src: local(""),
-    url("static/nunito-v16-latin-regular.woff2") format("woff2");
+    url("/nunito-v16-latin-regular.woff2") format("woff2");
 }
 
 :global(#app) {
     height: 100vh;
     text-align: center;
+}
+
+:global(button) {
+    display: inline-block;
+    outline: 0;
+    text-align: center;
+    cursor: pointer;
+    padding: 5px 10px;
+    border: 0;
+    color: #fff;
+    font-size: 17.5px;
+    border: 2px solid transparent;
+    border-color: #ffffff;
+    color: #ffffff;
+    background: transparent;
+    transition: background,color .1s ease-in-out;
+}
+                
+:global(button:hover) {
+    background-color: #ffffff;
+    color: #000000;
+}
+
+:global(button:disabled) {
+    color: #666666;
+    border-color:#666666;
+    background-color: unset;
+    cursor: not-allowed;
+}
+
+:global(button.selected) {
+    background: #007bff;
+    color: white;
+    border-color: #0056b3;
+}
+
+/* Use display: flow-root to create new Block Formatting Context.
+BFC prevents margins to overflow outside main and #app, breaking its 100vh.
+*/
+:global(main) {
+    display: flow-root;
 }
 
 </style>
