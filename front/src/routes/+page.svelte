9b11088cbd0b7050dad52bea40b2b5c92cb61a04
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-18 13:04:14 +0100

    Add subtitle and paragraph

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -7,6 +7,13 @@
 
 <h1>Artificial Suspects</h1>
 
+<p>
+A game exposing how different AI models form biased interpretations of people.</p>
+<p>
+Using the familiar structure of Unusual Suspects, the game lets an AI take the role of the witness.
+Every decision it makes highlights subtle biases that usually stay invisible behind the training data, weights and code.
+</p>
+
 <div class="menu">
     <button on:click={newGame}>Start Game</button>
 </div>
